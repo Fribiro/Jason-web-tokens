54b4f06b73caf4b2e2c17257fde569cf26695835
Festus Ribiro <festusribiro@gmail.com>
2021-01-15 00:00:08 +0300

    Basic setup

--- src/index.js
+++ src/index.js
@@ -7,8 +7,28 @@ const { verify } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
 
 //Register a user
-//login a user
-//logout a user
-//setup a protected route
-//get a new accesstoken with a refresh token
+//Login a user
+//Logout a user
+//Setup a protected route
+//Get a new accesstoken with a refresh token
 
+const server = express();
+
+//express middleware for easier cookie handling
+server.use(cookieParser());
+
+//setup cors
+server.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    })
+);
+
+//needed to read body data
+server.use(express.json());//to support json-encoded bodies
+server.use(express.urlencoded({ extended: true }));//support urlencoded bodies
+
+server.listen(process.env.PORT, () =>
+    console.log(`Server listenng on port ${process.env.PORT}`),
+); 
